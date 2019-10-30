35edd51269e9aba366c9ab343d607f5f5e195dba
wooferzfg <spapushin@gmail.com>
2019-10-29 19:27:58 -0500

    Add game_leagues_for_game_code method

--- resources/userResource.mjs
+++ resources/userResource.mjs
@@ -54,6 +54,33 @@ class UserResource {
       });
   }
 
+  game_leagues_for_game_code(gameCodes, cb = () => {}) {
+    // TODO: get stats from other users...
+    if (!Array.isArray(gameCodes)) {
+      gameCodes = [gameCodes];
+    }
+
+    return this.yf.api(
+      this.yf.GET,
+      `https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_codes=${gameCodes.join(
+        ","
+      )}/leagues?format=json`)
+      .then(data => {
+        const user = data.fantasy_content.users[0].user[0];
+        const leagues = mapUserLeagues(
+          data.fantasy_content.users[0].user[1].games
+        );
+
+        user.games = leagues;
+        cb(null, user);
+        return user;
+      })
+      .catch(e => { 
+        cb(e);
+        throw e;
+      });
+  }
+
   game_teams(gameKeys, cb = () => {}) {
     if (!Array.isArray(gameKeys)) {
       gameKeys = [gameKeys];
